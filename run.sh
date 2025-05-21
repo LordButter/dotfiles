34aa591ed78d3435518cda7be23dac38f647570c
Bastian Hollasch <bastian.hollasch98@gmx.de>
2025-05-21 22:08:15 +0200

    init

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+dry_run="0"
+init="0"
+grep=""
+
+while [[ $# -gt 0 ]]; do
+    echo "ARG: \"$1\""
+    if [[ "$1" == "--dry" ]]; then
+        dry_run="1"
+    elif [[ "$1" == "--init" ]]; then
+        init="1"
+    else
+        grep="$1"
+    fi
+    shift
+done
+
+log() {
+    if [[ "$dry_run" == "1" ]]; then
+        echo "[DRY_RUN]: $1"
+    else
+        echo "$1"
+    fi
+}
+
+if [[ "$init" == "1" ]]; then
+    init_dir=$(find init -mindepth 1 -maxdepth 1 -type f -executable)
+    echo "$init_dir"
+fi
+
+config_dir=$(find config -mindepth 1 -maxdepth 1 -exec basename {} \;)
+echo "$config_dir"
--- tmux/work.sh
+++
@@ -1,6 +0,0 @@
-./shell.sh
-./deployment.sh
-#./code_20.sh
-#./code_10.sh
-
-tmux attach-session -t 'work'
