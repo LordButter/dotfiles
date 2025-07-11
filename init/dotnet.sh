d41e45076736be7dd3139c3ebeddfc5a5bd9400e
LordButter <bastian.hollasch98@gmx.de>
2025-07-11 23:16:58 +0200

    adding dotnet.sh

---
+++ init/dotnet.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates libc6 libgcc-s1 libicu74 liblttng-ust1 libssl3 libstdc++6 zlib1g
+sudo apt-get install -y dotnet-sdk-8.0
+sudo apt-get install -y aspnetcore-runtime-8.0
